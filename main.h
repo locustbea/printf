2177c8983664cf02b37956ba1c920d2986e4626a
chidpeter <chidpeter@Larryxcell>
2023-08-25 21:17:58 +0100

    _Printf Project by Olalekan and Chidi

--- main.h
+++ main.h
@@ -1,54 +1,115 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdlib.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
 
-int _printf(const char *format, ...);
-int _putchar(char c);
-int _puts(char *str);
-void my_numbp(int n);
-int my_digitcounter(int n);
+#define my_longs 2
+#define my_shorts 1
+
+#define my_subsign 1
+#define my_addsign 2
+#define my_null 4
+#define my_hash 8
+#define my_space 16
 
 /**
- * struct my_flags - This struct contains my defined flags
- * @my_addsign: the '+' character
- * @my_space: the ' ' character
- * @my_hash: the '#' character
+ * struct my_format - Struct op
+ *
+ * @my_format: this is the format.
+ * @fn: The function associated.
+ */
+struct my_format
+{
+	char my_format;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+
+
+/**
+ * typedef struct my_format my_format_t - Struct op
+ *
+ * @my_format: The format.
+ * @fm_t: The function associated.
  */
 
-typedef struct my_flags
-{
-	int my_addsign;
-	int my_space;
-	int my_hash;
-} my_flags_t;
+typedef struct my_format my_format_t;
 
-int (*my_getp(char s))(va_list, my_flags_t *);
-int my_getf(char s, my_flags_t *b);
-char *my_conv(unsigned long int n, int b, int lcase);
+int _printf(const char *format, ...);
+int my_ph(const char *my_format, int *i,
+		va_list list, char my_buff[], int flags, int breadth, int my_pre, int size);
 
-typedef struct my_ph
-{
-	char c;
-	int (*b)(va_list ap, my_flags_t *b);
-} my_ph;
 
-/* Elements of arrayf[] */
-int percent_p(va_list l, my_flags_t *b);
-int binary_p(va_list l, my_flags_t *b);
-int character_p(va_list l, my_flags_t *b);
-int integer_p(va_list l, my_flags_t *b);
-int integer1_p(va_list l, my_flags_t *b);
-int octal_p(va_list l, my_flags_t *b);
-int address_p(va_list l, my_flags_t *b);
-int reverse_p(va_list l, my_flags_t *b);
-int rot13_p(va_list l, my_flags_t *b);
-int string_p(va_list l, my_flags_t *b);
-int stringbig_p(va_list l, my_flags_t *b);
-int unsigned_p(va_list l, my_flags_t *b);
-int hexa_p(va_list l, my_flags_t *b);
-int hexabig_p(va_list l, my_flags_t *b);
+/* chars and strings functions */
+int my_characterp(va_list my_stype, char my_buff[],
+			int flags, int breadth, int my_pre, int size);
+int my_stringp(va_list my_stype, char my_buff[],
+			int flags, int breadth, int my_pre, int size);
+int my_percentp(va_list my_stype, char my_buff[],
+			int flags, int breadth, int my_pre, int size);
+
+/* integer functions */
+int my_intp(va_list my_stype, char my_buff[],
+			int flags, int breadth, int my_pre, int size);
+int my_binaryp(va_list my_stype, char my_buff[],
+			int flags, int breadth, int my_pre, int size);
+int my_unsignedp(va_list my_stype, char my_buff[],
+			int flags, int breadth, int my_pre, int size);
+int my_octalp(va_list my_stype, char my_buff[],
+			int flags, int breadth, int my_pre, int size);
+int my_hexaD(va_list my_stype, char my_buff[],
+			int flags, int breadth, int my_pre, int size);
+int my_hexabig(va_list my_stype, char my_buff[],
+			int flags, int breadth, int my_pre, int size);
+
+int my_hexap(va_list my_stype, char map_to[],
+		char my_buff[], int flags, char flag_ch, int breadth, int my_pre, int size);
+
+/* Non printable characters function */
+int my_nonprintablep(va_list my_stype, char my_buff[],
+			int flags, int breadth, int my_pre, int size);
+
+/* Memory address functions*/
+int my_pointerp(va_list my_stype, char my_buff[],
+			int flags, int breadth, int my_pre, int size);
+
+/* Specifier functions */
+int my_getf(const char *format, int *i);
+int my_getw(const char *format, int *i, va_list list);
+int my_getp(const char *format, int *i, va_list list);
+int my_gets(const char *format, int *i);
+
+/* Reverse functions*/
+int my_reversep(va_list my_stype, char my_buff[],
+			int flags, int breadth, int my_pre, int size);
+
+/* Rot13 function*/
+int my_rot13s(va_list my_stype, char my_buff[],
+			int flags, int breadth, int my_pre, int size);
+
+/* breadth handler */
+int my_charwrite(char c, char my_buff[],
+			int flags, int breadth, int my_pre, int size);
+int my_nwrite(int is_positive, int ind, char my_buff[],
+			int flags, int breadth, int my_pre, int size);
+int my_nmwrite(int ind, char bfer[], int flags, int breadth, int my_pre,
+			int length, char padd, char extra_c);
+int my_pwrite(char my_buff[], int ind, int length,
+			int breadth, int flags, char padd, char extra_c, int my_startpadding);
+
+int my_uwrite(int is_negative, int ind,
+		char my_buff[],
+			int flags, int breadth, int my_pre, int size);
+
+
+int my_printables(char);
+int my_hexcodeapp(char, char[], int);
+int my_digit(char);
+
+long int my_convlong(long int num, int size);
+long int my_convunsigned(unsigned long int num, int size);
 
 #endif
---
+++ my_fn1.c
@@ -0,0 +1,179 @@
+#include "main.h"
+
+/**
+ * my_characterp - Prints a char
+ * @my_stype: List a of arguments
+ * @my_buff: my_buff array to handle print
+ * @flags:  Calculates active flags
+ * @breadth: breadth
+ * @my_pre: my_pre specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+
+int my_characterp(va_list my_stype, char my_buff[],
+			int flags, int breadth, int my_pre, int size)
+{
+	char c = va_arg(my_stype, int);
+
+	return (my_charwrite(c, my_buff, flags, breadth, my_pre, size));
+}
+
+/**
+ * my_stringp - Prints a string
+ * @my_stype: List a of arguments
+ * @my_buff: my_buff array to handle print
+ * @flags:  Calculates active flags
+ * @breadth: get breadth.
+ * @my_pre: my_pre specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+
+int my_stringp(va_list my_stype, char my_buff[],
+			int flags, int breadth, int my_pre, int size)
+{
+	int length = 0, i;
+	char *str = va_arg(my_stype, char *);
+
+	UNUSED(my_buff);
+	UNUSED(flags);
+	UNUSED(breadth);
+	UNUSED(my_pre);
+	UNUSED(size);
+	if (str == NULL)
+	{
+	str = "(null)";
+	if (my_pre >= 6)
+	str = "      ";
+	}
+	while (str[length] != '\0')
+	length++;
+	if (my_pre >= 0 && my_pre < length)
+	length = my_pre;
+	if (breadth > length)
+	{
+	if (flags & my_subsign)
+	{
+	write(1, &str[0], length);
+	for (i = breadth - length; i > 0; i--)
+	write(1, " ", 1);
+	return (breadth);
+	}
+	else
+	{
+	for (i = breadth - length; i > 0; i--)
+	write(1, " ", 1);
+	write(1, &str[0], length);
+	return (breadth);
+	}
+	}
+	return (write(1, str, length));
+}
+
+/**
+ * my_percentp - Prints a percent sign
+ * @my_stype: Lista of arguments
+ * @my_buff: my_buff array to handle print
+ * @flags:  Calculates active flags
+ * @breadth: get breadth.
+ * @my_pre: my_pre specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+
+int my_percentp(va_list my_stype, char my_buff[],
+			int flags, int breadth, int my_pre, int size)
+{
+	UNUSED(my_stype);
+	UNUSED(my_buff);
+	UNUSED(flags);
+	UNUSED(breadth);
+	UNUSED(my_pre);
+	UNUSED(size);
+	return (write(1, "%%", 1));
+}
+
+/**
+ * my_intp - Print int
+ * @my_stype: Lista of arguments
+ * @my_buff: my_buff array to handle print
+ * @flags:  Calculates active flags
+ * @breadth: get breadth.
+ * @my_pre: my_pre specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+
+int my_intp(va_list my_stype, char my_buff[],
+			int flags, int breadth, int my_pre, int size)
+{
+	int i = BUFF_SIZE - 2;
+	int is_negative = 0;
+	long int n = va_arg(my_stype, long int);
+	unsigned long int num;
+
+	n = my_convlong(n, size);
+	if (n == 0)
+	my_buff[i--] = '0';
+	my_buff[BUFF_SIZE - 1] = '\0';
+	num = (unsigned long int)n;
+	if (n < 0)
+	{
+	num = (unsigned long int)((-1) * n);
+	is_negative = 1;
+	}
+	while (num > 0)
+	{
+	my_buff[i--] = (num % 10) + '0';
+	num /= 10;
+	}
+	i++;
+	return (my_nwrite(is_negative, i, my_buff, flags, breadth, my_pre, size));
+}
+
+/**
+ * my_binaryp - Prints an unsigned number
+ * @my_stype: va_List of arguments
+ * @my_buff: this is a buffer array to handle print
+ * @flags:  Calculates active flags
+ * @breadth: get breadth.
+ * @my_pre: my_pre specification
+ * @size: Size specifier
+ * Return: Numbers of char printed.
+ */
+
+int my_binaryp(va_list my_stype, char my_buff[],
+			int flags, int breadth, int my_pre, int size)
+{
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
+
+	UNUSED(my_buff);
+	UNUSED(flags);
+	UNUSED(breadth);
+	UNUSED(my_pre);
+	UNUSED(size);
+
+	n = va_arg(my_stype, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+	m /= 2;
+	a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+	sum += a[i];
+	if (sum || i == 31)
+	{
+	char z = '0' + a[i];
+
+	write(1, &z, 1);
+	count++;
+	}
+	}
+	return (count);
+}
